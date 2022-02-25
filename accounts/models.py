24342be64729f67f1d3755fdfc77c7c62080e2b8
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-02-25 18:38:26 +0300

    initial commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth import validators
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext as _
