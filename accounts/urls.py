1c2d944d9867db4be55a219227ed9eeae2fb20b0
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-03-03 10:46:44 +0300

    Password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,28 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(
         template_name="accounts/logout.html"), name="logout"),
     path("signup/", dealerSignupView, name="dealerSignup"),
+    # Password Reset
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name="accounts/password_reset.html",
+             success_url='/password_reset/done/',
+             email_template_name="accounts/password_reset_email.html",
+             subject_template_name="accounts/password_reset_subject.txt"),
+         name="password_reset"),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html",),
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="accounts/password_reset_confirm.html",
+             success_url='/reset/done/'
+         ),
+         name="password_reset_confirm"
+         ),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/password_reset_complete.html"),
+        name="password_reset_complete"),
 ]
