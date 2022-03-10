6dbfe4ff2508f764708a4c720616da4f0c0a60e9
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-03-10 18:05:30 +0300

    Product model

--- farmers/models.py
+++ farmers/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from accounts.models import User
+from accounts.models import Farmer, TrackingModel
 from django.utils.translation import gettext as _
 # Create your models here.
 
@@ -14,3 +14,23 @@ class ProductCategory(models.Model):
 
     class Meta:
         verbose_name_plural = "Products Categories"
+
+
+class Product(models.Model):
+    item = models.CharField(_("item"), max_length=157)
+    farmer = models.ForeignKey(Farmer, on_delete=models.CASCADE)
+    quantity = models.FloatField(
+        default=0.00, help_text="Number of Kgs available")
+    image = models.ImageField(upload_to="products/%y%m/%d")
+    price = models.FloatField(_("price"))
+    timestamp = models.ForeignKey(TrackingModel,
+                                  on_delete=models.DO_NOTHING)
+    approve = models.BooleanField(_("approve"), default=True)
+    description = models.TextField(_("description"), blank=True, null=True)
+    on_stock = models.BooleanField(_("on stock"), default=True)
+
+    def __str__(self):
+        return str(self.item)
+
+    class Meta:
+        verbose_name_plural = "Products"
